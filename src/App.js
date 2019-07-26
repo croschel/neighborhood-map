874ba5a5e85f04152eead695bdbacb65fd006d1b
croschel <croschel000@gmail.com>
2019-07-26 01:20:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  initMap(){
+      let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+  loadScript(){
+    let scriptElement = this.createMapScript();
+    let scriptsPage = document.getElementsByTagName('script');
+    let script = scriptsPage[0];
+    script.parentNode.insertBefore(scriptElement,script);
+    window.initMap = this.initMap;
+  }
+
+  createMapScript(){
+    let mapScript = document.createElement("script");
+    const API_KEY = "AIzaSyDjkUnxuYPGTxhF5Zmbu65IN_8RbdRUg3Y";
+    mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+    mapScript.async = true;
+    mapScript.defer = true;
+    return mapScript;
+  }
+
+  render(){
+    this.loadScript();
+    return (
+      <div className="App" id="map">
+     
+      </div>
+    );
+  }
 }
-
 export default App;
